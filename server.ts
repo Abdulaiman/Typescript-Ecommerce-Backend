bbbc911c11c9a5a154797977e6f7d02d2446c2e7
aiman <abduldanmaihaja@gmail.com>
2022-10-08 19:52:52 +0100

    made connection to the database

--- server.ts
+++ server.ts
@@ -1,7 +1,22 @@
 const app = require("./app");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
-app.listen(8000, () => {
-  console.log("app listening on port 8000");
+dotenv.config({
+  path: "./config.env",
+});
+
+const DB = process.env.DATABASE?.replace(
+  "<password>",
+  `${process.env.DATABASE_PASSWORD}`
+);
+mongoose.connect(DB, { useNewUrlParser: true }).then(() => {
+  console.log("database connection successful");
+});
+
+const Port = process.env.PORT;
+app.listen(Port, () => {
+  console.log(`app listening on port ${Port}`);
 });
 
 export {};
